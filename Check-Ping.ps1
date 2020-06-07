c0b3cafd87b80d5cea5ac5243ea9cb22e5909b18
xtremepathak <xtreme.pathak@gmail.com>
2020-06-07 00:00:02 -0700

    Add files via upload

---
+++ Check-Ping.ps1
@@ -0,0 +1,28 @@
+Function Check-Ping
+{
+
+Param(
+      [String[]] $Computers
+     )
+
+if($Computers -eq $null)
+{
+import-module activedirectory
+$Computers = get-adcomputer -Filter {enabled -eq "true"} | select -ExpandProperty Name
+}
+$PingFail = @()
+$PingPass = @()
+foreach($comp in $Computers)
+{
+   if(Test-Connection $comp -Count 1 -ErrorAction SilentlyContinue)
+    {
+    $PingPass += $comp
+    }
+    else{
+    $PingFail += $comp
+    }
+}
+$return = "Ping Pass systems : `n $PingPass `nPing Failed systems : `n $PingFail"
+$return
+
+}
